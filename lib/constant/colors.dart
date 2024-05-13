a9f8185bc04321e0c2ed65a35e40fcb0ebc2ae44
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-05-14 03:21:06 +0330

    complate ConfirmMobileScrean

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -8,6 +8,9 @@ class MyColors{
   static Color red4=const Color(0xffa40016);
   static Color red5=const Color(0xff670009);
   static Color red6=const Color(0xff2e0002);
+  static Color grey500=const Color.fromRGBO(152, 162, 179, 1);
+  static Color grey300=const Color.fromRGBO(234, 236, 240, 1);
+  static Color grey400=const Color.fromRGBO(234, 236, 240, 1);
 
   static Color white=const Color(0xffEAEEEC);
   static Color grey=const Color(0xffC0C7C3);
---
+++ lib/screan/confirm_mobile_screan.dart
@@ -0,0 +1,167 @@
+import 'dart:async';
+import 'dart:ffi';
+
+import 'package:aviz_application/constant/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+class ConfirmMobileScrean extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _tController1 = TextEditingController();
+    TextEditingController _tController2 = TextEditingController();
+    TextEditingController _tController3 = TextEditingController();
+    TextEditingController _tController4 = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(
+              height: 70,
+              width: double.infinity,
+            ),
+            const Text(
+              "شماره موبایل",
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: "sb",
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "کد ورود پیامک شده را وارد کنید",
+              style: TextStyle(
+                color: MyColors.grey500,
+                fontFamily: "sb",
+                fontWeight: FontWeight.w400,
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  InputNumberTextField(
+                    focus: true,
+                    textController: _tController1,
+                    number: 1,
+                  ),
+                  InputNumberTextField(
+                    focus: false,
+                    textController: _tController2,
+                    number: 2,
+                  ),
+                  InputNumberTextField(
+                    focus: false,
+                    textController: _tController3,
+                    number: 3,
+                  ),
+                  InputNumberTextField(
+                    focus: false,
+                    textController: _tController4,
+                    number: 4,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  " ارسال مجدد کد ",
+                  style: TextStyle(
+                    color: MyColors.grey500,
+                    fontFamily: "sb",
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                "${Timer(const Duration(seconds: 5),() async {
+                  
+                },)}",
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: "sb",
+                    fontSize: 16,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+}
+
+class InputNumberTextField extends StatelessWidget {
+  bool focus;
+  int number;
+  TextEditingController textController = TextEditingController();
+  InputNumberTextField({
+    super.key,
+    required this.focus,
+    required this.textController,
+    required this.number,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 75,
+      decoration: BoxDecoration(
+        color: MyColors.grey300,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: TextField(
+        controller: textController,
+        style: const TextStyle(
+          fontFamily: "sm",
+          fontSize: 20,
+        ),
+        keyboardType: TextInputType.phone,
+        onChanged: (value) {
+          if ((value.isNotEmpty) && (number != 4)) {
+            FocusScope.of(context).nextFocus();
+          }
+        },
+        maxLines: 1,
+        maxLength: 1,
+        textAlign: TextAlign.center,
+        autofocus: focus,
+        decoration: InputDecoration(
+          counterText: "",
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: MyColors.red3,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
