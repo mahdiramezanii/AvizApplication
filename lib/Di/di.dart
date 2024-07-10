14280ce42adf131f475e0f34d45d1b6855d7e850
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-07-10 11:10:46 +0330

    update locator

--- lib/Di/di.dart
+++ lib/Di/di.dart
@@ -2,6 +2,7 @@ import 'package:aviz_application/Features/Home/bloc/home_bloc.dart';
 import 'package:aviz_application/Features/Home/data/datasource/home_datasource.dart';
 import 'package:aviz_application/Features/Home/data/repository/home_repository.dart';
 import 'package:aviz_application/Features/search/data/datasource/search_datasorce.dart';
+import 'package:aviz_application/Features/search/data/repository/serach_repository.dart';
 import 'package:aviz_application/NetworkUtil/dio_provider.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
@@ -19,6 +20,8 @@ Future<void> initLocator() async {
       () => HomeRepository(dataSource: locator.get()));
 
   locator.registerFactory<ISearchDataSource>(() => SearchRemoteDataSource(dio: locator.get()));
+
+  locator.registerFactory<ISerachRepository>(() => SerachRepository(dataSource: locator.get()));
 
 
       locator.registerSingleton<HomeBloc>(HomeBloc(repository: locator.get()));
